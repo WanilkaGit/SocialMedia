49485659a29035b2dac899833f8f8934e1dc5f75
WanilkaGit <.email@example.com>
2025-03-08 17:44:40 +0200

    End of work pre lesson

---
+++ courceszone_sys/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Lessons, Cources
+
+
+class CreatCourceForm():
+    class Meta:
+        model = Cources
+        exclude = ["c_uploaded", "c_lessons", "c_rating", "c_bye_count"]
+        help_texts = {
+            "c_title": "Введіть повну назву",
+        }
+
+class CreateLessonForm():
+    class Meta:
+        model = Lessons
+        exclude = ["lc_id"]
+        help_texts = {
+            "l_title": "Введіть повну назву книги.",
+        }
--- courceszone_sys/views.py
+++ courceszone_sys/views.py
@@ -1,57 +1,39 @@
 from django.shortcuts import render, redirect
 from .models import Cources, Lessons
 from django.contrib import messages
+from .form import CreatCourceForm, CreateLessonForm
 
 # Create your views here.
 
 def ncourse_view(request):
     if request.method == "POST":
-        c_title = request.POST.get("c-title")
-        c_description = request.POST.get("c-description")
-        c_type = request.POST.get("c-type")
-        c_prize = request.POST.get("c-prize")
-        c_addeting_files = request.POST.get("c-addeting-files")
-        c_files = request.POST.get("c-files")
-        c_pay_choices = request.POST.get("c-pay-choices")
-
-        if c_title and c_type and c_addeting_files and c_pay_choices:
-            ncourse = Cources(
-                c_title=c_title,
-                c_description=c_description,
-                c_type=c_type,
-                c_prize=c_prize,
-                c_addeting_files=c_addeting_files,
-                c_files=c_files,
-                c_pay_choices=c_pay_choices,
-            )
-            ncourse.save()
+        form = CreatCourceForm(request.POST)
+        if form.is_valid():
+            ncourse = form.save()
             messages.success(request, "Курс додано до списку")
             return redirect("index_o_info")
         else:
-            messages.error(request, "Capitan, we have a trouble, you furgot to add something")
+            messages.error(request, "Capitan, we have a trouble, you forgot to add something")
+    else:
+        form = CreatCourceForm()  # Створюємо нову форму для GET запиту
+
+    return render(request, 'template_name.html', {'form': form})  # Змініть 'template_name.html' на ваш шаблон
 
 def nlesson_view(request):
     if request.method == "POST":
-        l_file = request.FILES.get("l-file")
-        l_title = request.POST.get("l-title")
-        l_description = request.POST.get("l-description")
-        l_time = request.POST.get("l-time")
-        lc_id = request.POST.get("l-id")
-        l_course = request.POST.get("l_course")
-        if l_title and lc_id and l_course:
-            nlesson = Lessons(
-                l_title=l_title,
-                l_description=l_description,
-                l_time=l_time,
-                lc_id=lc_id,
-            )
-            nlesson.save()
-            addicted_course = Cources.objects.get(id=l_course)
+        c_id = request.POST.get("c_id")
+        form = CreateLessonForm(request.POST)
+        if form.is_valid():
+            nlesson = form.save()
+            addicted_course = Cources.objects.get(id=c_id)
             addicted_course.c_lessons.add(nlesson)
             addicted_course.save()
             return redirect("index_o_info")
         else:
-            messages.error(request, "Capitan, SOS, we have trouble some requirment fields is NONE")
+            messages.error(request, "Capitan, SOS, we have trouble form is not valid")
+    else:
+        messages.error(request, "Capitan, method not POST you fucken DUMN")
+    messages.error(request, "Capitan, SOS, we have trouble some requirment fields is NONE")
 
 def videocos_view(request):
     cources = Cources.objects.filter(c_type="video")
